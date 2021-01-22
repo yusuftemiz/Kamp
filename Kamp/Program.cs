640796c8f879b7bcc80450ca4f43b7e48c424904
yusuf temiz <yusuftemiz22@gmail.com>
2021-01-22 18:32:28 +0300

    Proje dosyası ekle.

---
+++ Kamp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace KampOOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStocks = 3;
+
+            Product product2 = new Product { Id = 2, CategoryId = 5, UnitsInStocks = 5, 
+                                            UnitPrice = 35, ProductName = "Kalem" };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+        }
+    }
+}
