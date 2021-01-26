40bb56ae90366e850a0e60e1cbd7b568bfe91500
yusuf temiz <yusuftemiz22@gmail.com>
2021-01-26 16:59:13 +0300

    23 Ocak

---
+++ OOP2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Musteri musteri1 = new Musteri();
+            //musteri1.Adi = "Engin";
+            //musteri1.Soyadi = "Demirog";
+            //musteri1.Id = 1;
+            //musteri1.TcNo = "22222222";
+            //musteri1.MusteriNo = "12342";
+            //musteri1.SirketAdi = "?";
+
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "1123";
+            musteri1.Adi = "Engin";
+            musteri1.Soyadi = "Demirog";
+            musteri1.TcNo = "12314354";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "123243";
+            musteri2.SirketAdi = "kodlama.io";
+            musteri2.VergiNo = "2134";
+
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            CustomerManager musteriManager = new CustomerManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri3);
+
+
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ıhtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(ıhtiyacKrediManager, new DatabaseLoggerService);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ıhtiyacKrediManager, tasitKrediManager };
+
+            basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+        }
+    }
+}
